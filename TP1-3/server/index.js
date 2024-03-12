ef73c63f91710427119e8e65e1e31c4332dce2a7
Daniel Huf <dani.s.huf@gmail.com>
2024-03-12 23:44:21 +0100

    added button to delete message

--- TP1-3/server/index.js
+++ TP1-3/server/index.js
@@ -7,126 +7,128 @@ var allUsernames = ["Jimmy", "Dani", "Gymnast24"]
 var allDatetimes = [now, now, now];
 
 app.use((req, res, next) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    next();
+	res.setHeader('Content-Type', 'text/html; charset=utf-8');
+	next();
 });
 
 // Pour s'assurer que l'on peut faire des appels AJAX au serveur
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Username, Datetime");
-    res.header("Access-Control-Expose-Headers", "Username, Datetime");
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Username, Datetime");
+	res.header("Access-Control-Expose-Headers", "Username, Datetime");
 
-    next();
+	next();
 });
 
 // Ici faut faire faire quelque chose à notre app...
 // On va mettre les "routes"  == les requêtes HTTP acceptéés par notre application.
 
 app.get("/", function(req, res) {
-    res.send("Hello")
+	res.send("Hello")
 })
 
 app.get("/test/*", function(req, res) {
-    // res.send("Path after /test/ : " + req.params[0]);
-    // res.send({"a": 1, "b" : 2});
-    // res.send(["Hello", "World"]);
-    // res.send(42);
-    res.send(req.url.substr(6));
+	// res.send("Path after /test/ : " + req.params[0]);
+	// res.send({"a": 1, "b" : 2});
+	// res.send(["Hello", "World"]);
+	// res.send(42);
+	res.send(req.url.substr(6));
 });
 
 app.get('/blah*', function(req, res) {
-    console.log(req.query)
+	console.log(req.query)
 });
 
 app.get('/cpt/query', function(req, res) {
-    res.json({ "counter": counter });
+	res.json({ "counter": counter });
 });
 
 app.get('/cpt/inc', function(req, res) {
-    var v = req.query.v;
+	var v = req.query.v;
 
-    if (!v) {
-        counter++;
-        res.json({ "code": 0 });
-    } else {
-        if (v.match(/^-?\d+$/)) {
-            counter += parseInt(v, 10);
-            res.json({ "code": 0 });
-        } else {
-            res.json({ "code": -1 });
-        }
-    }
+	if (!v) {
+		counter++;
+		res.json({ "code": 0 });
+	} else {
+		if (v.match(/^-?\d+$/)) {
+			counter += parseInt(v, 10);
+			res.json({ "code": 0 });
+		} else {
+			res.json({ "code": -1 });
+		}
+	}
 });
 
 // ======================================== APPLICATION ========================================
 
 // Fonction pour vérifier si un id est valide
 function checkId(msgId) {
-    const id = parseInt(msgId, 10);
+	const id = parseInt(msgId, 10);
 
-    // L'id doit être un nombre et doit être dans l'intervalle [0, allMsgs.length[
-    if (isNaN(id) || id < 0 || id >= allMsgs.length) {
-        return -1;
-    }
+	// L'id doit être un nombre et doit être dans l'intervalle [0, allMsgs.length[
+	if (isNaN(id) || id < 0 || id >= allMsgs.length) {
+		return -1;
+	}
 
-    return id;
+	return id;
 }
 
 app.get('/msg/get/*', function(req, res) {
-    const msgIdStr = req.url.substr(9);
+	const msgIdStr = req.url.substr(9);
+	const msgId = checkId(msgIdStr);
 
-    if (checkId(msgIdStr) === -1) {
-        res.json({ "code": 0 })
-    }
+	if (msgId === -1) {
+		res.json({ "code": 0 })
+	}
 
-    res.setHeader('Username', JSON.stringify(allUsernames[msgIdStr]));
-    res.setHeader('Datetime', JSON.stringify(allDatetimes[msgIdStr]));
-    res.json({ "code": 1, "msg": allMsgs[msgIdStr] });
+	res.setHeader('Username', JSON.stringify(allUsernames[msgId]));
+	res.setHeader('Datetime', JSON.stringify(allDatetimes[msgId]));
+	res.json({ "code": 1, "msg": allMsgs[msgId] });
 });
 
 app.get('/msg/nber', function(req, res) {
-    res.json(allMsgs.length)
+	res.json(allMsgs.length)
 });
 
 app.get('/msg/getAll', function(req, res) {
-    res.setHeader('Username', JSON.stringify(allUsernames));
-    res.setHeader('Datetime', JSON.stringify(allDatetimes));
-    res.json(allMsgs);
+	res.setHeader('Username', JSON.stringify(allUsernames));
+	res.setHeader('Datetime', JSON.stringify(allDatetimes));
+	res.json(allMsgs);
 });
 
 app.get('/msg/post/*', function(req, res) {
-    const msg = unescape(req.url.substr(10));
+	const msg = unescape(req.url.substr(10));
 
-    if (req.headers.username) {
-        const username = req.headers.username;
-        allUsernames.push(username);
-    } else {
-        allUsernames.push("Anonymous")
-    }
+	if (req.headers.username) {
+		const username = req.headers.username;
+		allUsernames.push(username);
+	} else {
+		allUsernames.push("Anonymous")
+	}
 
-    if (req.headers.datetime) {
-        const datetime = req.headers.datetime;
-        allDatetimes.push(datetime);
-    } else {
-        allDatetimes.push(new Date().toISOString())
-    }
+	if (req.headers.datetime) {
+		const datetime = req.headers.datetime;
+		allDatetimes.push(datetime);
+	} else {
+		allDatetimes.push(new Date().toISOString())
+	}
 
-    allMsgs.push(msg);
-    res.json({ "newMessageId": allMsgs.length - 1 })
+	allMsgs.push(msg);
+	res.json({ "newMessageId": allMsgs.length - 1 })
 });
 
 app.get('/msg/del/*', function(req, res) {
-    const msgIdStr = req.url.substr(9);
+	const msgIdStr = req.url.substr(9);
+	const msgId = checkId(msgIdStr);
 
-    if (!checkId(msgIdStr)) {
-        res.json({ "code": 0 })
-    } else {
-        allMsgs.splice(msgId, 1);
-        allUsernames.splice(msgId, 1);
-        allDatetimes.splice(msgId, 1);
-        res.json({ "code": 1 });
-    }
+	if (msgId === -1) {
+		res.json({ "code": 0 })
+	} else {
+		allMsgs.splice(msgId, 1);
+		allUsernames.splice(msgId, 1);
+		allDatetimes.splice(msgId, 1);
+		res.json({ "code": 1 });
+	}
 });
 
 app.listen(8080); //commence à accepter les requêtes
